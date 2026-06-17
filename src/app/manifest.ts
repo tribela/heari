06716a715694a6c5e9164ccf72d7e7369aa1fdf4
Jeong Arm <kjwonmail@gmail.com>
2026-06-18 03:21:21 +0900

    PWA icon is maskable

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -14,11 +14,13 @@ export default function manifest(): MetadataRoute.Manifest {
         src: "/pwa-icon?size=192",
         sizes: "192x192",
         type: "image/png",
+        purpose: "any maskable" as any,
       },
       {
         src: "/pwa-icon?size=512",
         sizes: "512x512",
         type: "image/png",
+        purpose: "any maskable" as any,
       },
     ],
   };
